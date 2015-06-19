881c705f1dfe0404426baaf745cc6c0c51d46ea3
Robert Whitney <xnite@xnite.me>
2015-06-18 23:44:27 -0500

    ...

---
+++ content-page.php
@@ -0,0 +1,14 @@
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><h1 class="post-title"><?php the_title(); ?></h1></a>
+		<p class="meta-info">Posted on <?php the_time( 'm/d/Y' ); ?> by <?php the_author(); ?> <?php edit_post_link(); ?></p>
+		<p class="post-body">
+			<?php the_content(); ?>
+			<?php wp_link_pages(); ?>
+		</p>
+		<footer class="post-footer">
+
+			<div id="comments">
+				<?php comments_template(); ?>
+			</div>
+		</footer>
+</div>
