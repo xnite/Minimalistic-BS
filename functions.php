bd2cbda779e4a40d03925a3448e281ff956c9987
Robert Whitney <xnite@xnite.me>
2015-06-18 23:54:20 -0500

    infinite scroll support

--- functions.php
+++ functions.php
@@ -12,4 +12,18 @@ require_once('wp_bootstrap_navwalker.php');
 		)
 	);
 	register_nav_menu( 'primary', 'Primary' );
+	
+	function minimalisticBS_init() {
+		$mytheme_inf_options=array(
+			'type'				=> 'scroll',
+			'footer_widgets'	=> false,
+			'container'			=> 'content',
+			'footer'			=> true,
+			'wrapper'			=> false,
+			'render'			=> false,
+			'posts_per_page'	=> 15,
+		);
+		add_theme_support( 'infinite-scroll', $mytheme_inf_options );
+	}
+	add_action( 'init', 'minimalisticBS_init' );
 ?>
