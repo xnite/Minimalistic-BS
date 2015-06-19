c962fd9e6c7ba6c20898d925621d41dac50e254e
Robert Whitney <xnite@xnite.me>
2015-06-18 23:45:15 -0500

    ...

--- single.php
+++ single.php
@@ -3,20 +3,7 @@
 <div class="content">
 	<?php
 		while (have_posts()) : the_post();
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><h1 class="post-title"><?php the_title(); ?></h1></a>
-					<p class="meta-info">Posted on <?php the_time( 'm/d/Y' ); ?> by <?php the_author(); ?> <?php edit_post_link(); ?></p>
-					<p class="post-body">
-						<?php the_excerpt(); ?>
-						<?php wp_link_pages(); ?>
-					</p>
-					<footer class="post-footer">
-
-						<div id="comments">
-							<?php comments_template(); ?>
-						</div>
-					</footer>
-			</div>
+			get_template_part( 'content-page', get_post_format() ); 
 		endwhile; 
 	?>
 </div>
