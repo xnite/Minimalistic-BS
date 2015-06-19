ec3751abf5369a3e9831e7129ad6c40b233d5a9e
Robert Whitney <xnite@xnite.me>
2015-06-19 15:28:53 -0500

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div id="content" class="content text-center">
+	<img src="<?php echo get_template_directory_uri(); ?>/img/dead-link.png" />
+	<h1>Woops!</h1>
+	<p>The page, post or file that you were looking for could not be found.<br />
+	Try starting back at the <a href="/">Home page</a></p>
+</div>
+<?php wp_footer();?>
+<?php get_footer(); ?>
