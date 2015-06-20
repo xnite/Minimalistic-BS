562c9b87a471f9e8f709f139afc2a2e0829260ac
Robert Whitney <xnite@xnite.me>
2015-06-19 19:34:19 -0500

    Fix category display

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><h1 class="post-title"><?php the_title(); ?></h1></a>
-		<p class="meta-info">Posted on <?php the_time( 'm/d/Y' ); ?> by <?php the_author(); ?> in <?php the_category( '|', 'single' ); ?> <?php edit_post_link(); ?></p>
+		<p class="meta-info">Posted on <?php the_time( 'm/d/Y' ); ?> by <?php the_author(); ?> in <?php the_category( ' | ' ); ?> <?php edit_post_link(); ?></p>
 		<p class="post-body">
 			<?php the_excerpt(); ?>
 			<?php wp_link_pages(); ?>
